3d01063dde5b92e2e7576e31fee75b6e4e82ab5b
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-06-24 18:07:09 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -279,52 +279,55 @@ wget -O running "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/running
 wget -O clearcache "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/clearcache.sh"
 wget -O menu-trgo "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-trgo.sh"
 wget -O menu-trojan "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-trojan.sh"
+wget -O cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.sh"
 
 # menu ssh ovpn
 wget -O menu-ssh "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/tendang.sh"
-
-# menu system
-wget -O menu-set "https://raw.githubusercontent.com/Andyyuda/1/main/menu/menu-set.sh"
-wget -O menu-domain "https://raw.githubusercontent.com/Andyyuda/1/main/menu/menu-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/add-host.sh"
-wget -O port-change "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-change.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/artanodrop/v4/main/xray/certv2ray.sh"
-wget -O menu-webmin "https://raw.githubusercontent.com/Andyyuda/1/main/menu/menu-webmin.sh"
-wget -O speedtest "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/artanodrop/v4/main/menu/about.sh"
-wget -O auto-reboot "https://raw.githubusercontent.com/artanodrop/v4/main/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/artanodrop/v4/main/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/artanodrop/v4/main/menu/bw.sh"
-wget -O up "https://raw.githubusercontent.com/Andyyuda/v4/main/up.sh"
-
-# change port
-wget -O port-ssl "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-ssl.sh"
-wget -O port-ovpn "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-ovpn.sh"
-
-
-wget -O xp "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/xp.sh"
-wget -O acs-set "https://raw.githubusercontent.com/artanodrop/v4/main/acs-set.sh"
-
-wget -O sshws "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/sshws.sh"
-wget -O status "https://raw.githubusercontent.com/artanodrop/v4/main/status.sh"
-wget -O menu-bckp "https://raw.githubusercontent.com/Andyyuda/1/main/menu/menu-bckp.sh"
-wget -O backup "https://raw.githubusercontent.com/artanodrop/v4/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/artanodrop/v4/main/backup/restore.sh"
-
-wget -O jam "https://raw.githubusercontent.com/artanodrop/v4/main/jam.sh"
+wget -O renew "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/tendang.sh"
+wget -O slow "https://raw.githubusercontent.com/Andyyuda/ver3/main/slow.sh"
+wget -O usernew "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/trial.sh"
 wget -O user-lock "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/user-lock.sh"
 wget -O user-unlock "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/user-unlock.sh"
-wget -O cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.sh"
+
+# menu system
+wget -O menu-set "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-set.sh"
+wget -O menu-domain "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/add-host.sh"
+wget -O port-change "https://raw.githubusercontent.com/Andyyuda/v4/main/port/port-change.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/Andyyuda/v4/main/xray/certv2ray.sh"
+wget -O menu-webmin "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-webmin.sh"
+wget -O speedtest "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/about.sh"
+wget -O auto-reboot "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/bw.sh"
+
+# change port
+wget -O port-ssl "https://raw.githubusercontent.com/Andyyuda/v4/main/port/port-ssl.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/Andyyuda/v4/main/port/port-ovpn.sh"
+wget -O genssl "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/genssl.sh"
+
+
+wget -O xp "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/xp.sh"
+wget -O acs-set "https://raw.githubusercontent.com/Andyyuda/v4/main/acs-set.sh"
+
+wget -O sshws "https://raw.githubusercontent.com/Andyyuda/v4/main/ssh/sshws.sh"
+wget -O status "https://raw.githubusercontent.com/Andyyuda/v4/main/status.sh"
+wget -O menu-bckp "https://raw.githubusercontent.com/Andyyuda/v4/main/menu/menu-bckp.sh"
+wget -O backup "https://raw.githubusercontent.com/Andyyuda/v4/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/Andyyuda/v4/main/backup/restore.sh"
+
+wget -O jam "https://raw.githubusercontent.com/Andyyuda/v4/main/jam.sh"
+wget -O up "https://raw.githubusercontent.com/Andyyuda/v4/main/up.sh"
+wget -q -O /usr/bin/up "https://raw.githubusercontent.com/Andyyuda/v4/main/up.sh" && chmod +x /usr/bin/up
 
 chmod +x menu
 chmod +x menu-trial
@@ -346,6 +349,12 @@ chmod +x delete
 chmod +x autokill
 chmod +x ceklim
 chmod +x tendang
+chmod +x user-lock
+chmod +x user-unlock
+chmod +x cf
+chmod +x up
+chmod +x slow
+chmod +x genssl
 
 chmod +x menu-set
 chmod +x menu-domain
@@ -358,7 +367,6 @@ chmod +x about
 chmod +x auto-reboot
 chmod +x restart
 chmod +x bw
-chmod +x up
 
 chmod +x port-ssl
 chmod +x port-ovpn
@@ -371,9 +379,6 @@ chmod +x menu-bckp
 chmod +x backup
 chmod +x restore
 chmod +x jam
-chmod +x user-lock
-chmod +x user-unlock
-chmod +x cf
 cd
 
 
